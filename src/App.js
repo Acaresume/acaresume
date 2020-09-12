eae7c70ed031a9c883b808dcd261d9a0fc8e0a66
Rahul Sabharwal <sabharwalrahul2000@gmail.com>
2020-09-12 16:49:16 +0530

    resume template added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Particles from "react-particles-js"
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +7,120 @@ import {
   Link
 } from "react-router-dom";
 import GetStarted from './components/getStarted';
-import './css/app.css';
+import './css/App.css';
 
 export default function App() {
   return (
+    <div className="App">
+    <Particles className="particles" params={
+                {
+                    "particles": {
+                        "number": {
+                            "value": 50,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
+                        },
+                        "color": {
+                            "value": "#ffffff"
+                        },
+                        "shape": {
+                            "type": "circle",
+                            "stroke": {
+                                "width": 0,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 5
+                            },
+                        },
+                        "opacity": {
+                            "value": 0.6,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 2,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 10,
+                                "size_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false,
+                            "distance": 150,
+                            "color": "#ffffff",
+                            "opacity": 0.4,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 1,
+                            "direction": "top",
+                            "random": true,
+                            "straight": true,
+                            "out_mode": "out",
+                            "bounce": true,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 600
+                            }
+                        }
+                    },
+                    "interactivity": {
+                        "detect_on": "canvas",
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "bubble"
+                            },
+                            "onclick": {
+                                "enable": false,
+                                "mode": "repulse"
+                            },
+                            "resize": true
+                        },
+                        "modes": {
+                            "grab": {
+                                "distance": 400,
+                                "line_linked": {
+                                    "opacity": 1
+                                }
+                            },
+                            "bubble": {
+                                "distance": 250,
+                                "size": 0,
+                                "duration": 2,
+                                "opacity": 0,
+                                "speed": 6
+                            },
+                            "repulse": {
+                                "distance": 400,
+                                "duration": 0.4
+                            },
+                            "push": {
+                                "particles_nb": 4
+                            },
+                            "remove": {
+                                "particles_nb": 2
+                            }
+                        }
+                    },
+                    "retina_detect": true
+                }
+            } />
     <Router>
-      <div>
+      <div className="navbar">
         <nav>
           <ul>
             <li>
@@ -41,6 +150,7 @@ export default function App() {
         </Switch>
       </div>
     </Router>
+    </div>
   );
 }
 
